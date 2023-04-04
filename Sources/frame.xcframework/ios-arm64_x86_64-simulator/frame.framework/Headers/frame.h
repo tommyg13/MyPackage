c250d52827237cc35c5594016d20f45b90a2f048
tgazikas <tgazikas@rpay.ai>
2023-04-04 19:40:09 +0300

    init

---
+++ Sources/frame.xcframework/ios-arm64_x86_64-simulator/frame.framework/Headers/frame.h
@@ -0,0 +1,18 @@
+//
+//  frame.h
+//  frame
+//
+//  Created by thomas on 4/4/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for frame.
+FOUNDATION_EXPORT double frameVersionNumber;
+
+//! Project version string for frame.
+FOUNDATION_EXPORT const unsigned char frameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <frame/PublicHeader.h>
+
+
