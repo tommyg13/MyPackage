5f6b5d5f691d482c07ddd3912393c0f93f5f024b
tgazikas <tgazikas@rpay.ai>
2023-04-05 19:22:15 +0300

    test 3

---
+++ frametestWrapper/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by thomas on 5/4/23.
+//
+
+import Foundation
+import SocketIO
+
+public struct TestS{
+    public static func testFun(){
+        print("working 2`")
+    }
+}
