a744b753eece3507ec8f0bcf92a2936c489b7143
tgazikas <tgazikas@rpay.ai>
2023-04-05 19:12:30 +0300

    test

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MyPackage",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -16,14 +16,23 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/socketio/socket.io-client-swift", .upToNextMinor(from: "15.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+          name: "frametestWrapper",
+          dependencies:[
+              .target(name: "frametest"),
+              .product(name: "SocketIO", package: "socket.io-client-swift")
+          ],
+          path: "frametestWrapper"
+        ),
         .binaryTarget(
-            name: "frametest",
-            path: "./Sources/frametest.xcframework")
+          name: "frametest",
+          path: "./Sources/frametest.xcframework"
+        )
     ]
-
 )
+
